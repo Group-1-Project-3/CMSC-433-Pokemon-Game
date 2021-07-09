48111bce701bc0699c4d78fc5d4a42cd8e2aeb61
Kes Johnson <kes.j.johnson@gmail.com>
2021-07-09 15:36:59 -0400

    init battle file

---
+++ battle.js
@@ -0,0 +1,22 @@
+function startBattle(user, comp) {
+    // get starting pokemon
+    user-pokemon = user[1]
+    comp-pokemon = comp[1]
+    // start graphics for battle
+    // while none faint, fight with those pokemon
+    while (user-pokemon['hp'] != 0 && comp-pokemon['hp'] != 0){
+        // wait for user choice
+        // user attacks
+        // comp attacks
+        // if one has fainted, replace with next
+        // if no more, declare winner
+    }
+}
+
+function attack(attack-pokemon, defense-pokemon){
+    // get attack value
+    // get denfence value
+    // calculate damage
+    // initiate graphics
+    // return new stats for each
+}

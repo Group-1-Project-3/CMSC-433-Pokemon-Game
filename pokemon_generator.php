49f55a005444ff67bf4ae312809584916a13ed68
nguyenlenh92 <lnguyen6@umbc.edu>
2021-07-11 20:24:37 -0400

    first iterator of pokemon generator

---
+++ pokemon_generator.php
@@ -0,0 +1,21 @@
+<?php
+    $dbhost = "127.0.0.1";
+    $dbuser = "root";
+    $db = "pokemonDB";
+
+    $con = mysqli_connect($dbhost, $dbuser, "", $db);
+    $random = rand(1, 166);
+    $query = "SELECT entry_id, pokemonid, name, type1, type2, totalstat, hp, attack, defense, specialattack, specialdefense, speed, generation, legendarystatus FROM pokemon_data WHERE (entry_id = $random)";
+
+    $result = mysqli_query($con, $query);
+
+    $array = mysqli_fetch_row($result);
+
+    echo json_encode($array);
+
+
+
+
+
+
+?>

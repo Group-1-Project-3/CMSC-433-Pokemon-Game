040768d3086558b6cc838a04a567d2d110bc2183
nguyenlenh92 <lnguyen6@umbc.edu>
2021-07-12 16:47:14 -0400

    Pokemon Class and get pokemon data script

--- pokemon_class.js
+++ pokemon_class.js
@@ -1,3 +1,4 @@
+import { TYPES, TYPE_ORDER, TYPE_CHART } from "./type-chart.js";
 class Pokemon {
 
     constructor(pokemon_name, type1, type2, totalstat, hp, attack, defense, spattack, spdefense, speed, level){
@@ -16,17 +17,33 @@ class Pokemon {
         this.level = level;
     }
 
-    calcRequiredXP(level){    // fast leveling
+    calcRequiredXP(level){
+        // fast leveling
         return 4 * (level ** 3) / 5;
     }
 
-    calcDamage(attack, targetPokemon){
-        var random = Math.floor(Math.random() * (1.00 - 0.85)) + 0.85;
-        // var type = TYPE_CHART[this.type1][TYPE_ORDER[targetPokemon.type1]];
-        return ((((2 * this.level) / 5 + 2) * (attack / targetPokemon.defense)) / 50 + 2) * random;
+    calcDamage(targetPokemon, typeOfAttack){
+        if (typeOfAttack == "physical"){
+            var random = Math.floor(Math.random() * (1.00 - 0.85)) + 0.85;
+            // var type = TYPE_CHART[this.type1][TYPE_ORDER[targetPokemon.type1]];
+            var type = 1;
+            var critical = 1;
+            return ((2 * this.level + 10) / 250 * (this.attack)/(targetPokemon.defense) * 100 + 2) * type * random * critical;
+        }
+        else if (typeOfAttack == "special"){
+            var random = Math.floor(Math.random() * (1.00 - 0.85)) + 0.85;
+            // var type = TYPE_CHART[this.type1][TYPE_ORDER[targetPokemon.type1]];
+            var type = 1;
+            var critical = 1;
+            return ((2 * this.level + 10) / 250 * (this.attack)/(targetPokemon.defense) * 100 + 2) * type * random * critical;
+        }
+        else {
+            console.log("Please enter correct type of attack");
+            return -1;
+        }
     }
 
-    catch(){
+    catchPokemon(){
         var f = (this.hpmax * 255 * 4) / (this.hp * 12);
         var m = Math.floor(Math.random() * 255);
         if (f >= m){
@@ -37,3 +54,4 @@ class Pokemon {
         }
     }
 }
+export { Pokemon };
--- proj3.js
+++ proj3.js
@@ -1,19 +1,29 @@
-$(document).ready(function(){
-    $("#mybutton").click(function(){
-        $.get("pokemon_generator.php", function(data){
 
-            var pokemon = jQuery.parseJSON(data);
-            var pokemon_id = pokemon[1];
-            var pokemon_name = pokemon[2];
-            var type1 = pokemon[3];
-            var type2 = pokemon[4];
-            var totalstat = pokemon[5];
-            var hp = pokemon[6];
-            var attack = pokemon[7];
-            var defense = pokemon[8];
-            var spattack = pokemon[9];
-            var spdefense = pokemon[10];
-            var speed = pokemon[11];
+import { Pokemon } from "./pokemon_class.js";
+
+$(document).ready(function(){
+    $.get("get_pokemon_data.php?pokemon_name=Charmander", function(data){
+        var pokemon = jQuery.parseJSON(data);
+        var pokemon_id = pokemon[1];
+        var pokemon_name = pokemon[2];
+        var type1 = pokemon[3];
+        var type2 = pokemon[4];
+        var totalstat = pokemon[5];
+        var hp = pokemon[6];
+        var attack = pokemon[7];
+        var defense = pokemon[8];
+        var spattack = pokemon[9];
+        var spdefense = pokemon[10];
+        var speed = pokemon[11];
+
+        var pok = new Pokemon(pokemon_name, type1, type2,totalstat, hp, attack, defense, spattack, spdefense, speed, 20);
+        var pokcurrentHP = pok.hp;
+        console.log("attack is ", pokemon[7]);
+
+        var pok2 = new Pokemon(pokemon_name, type1, type2,totalstat, hp, attack, defense, spattack, spdefense, speed, 5);
+        console.log("defense is ", pokemon[8]);
+        $("#mybutton").click(function(p){
+            console.log(pokcurrentHP -= pok.calcDamage(pok2, "special"));
         });
     });
 });

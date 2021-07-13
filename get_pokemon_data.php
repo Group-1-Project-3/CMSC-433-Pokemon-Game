113c269a9099d5bdc55092d609461cdef1dd4849
krisfay25 <87204399+krisfay25@users.noreply.github.com>
2021-07-13 16:02:25 -0400

    First iteration of poke starter

---
+++ get_pokemon_data.php
@@ -0,0 +1,17 @@
+<?php
+    $dbhost = "127.0.0.1";
+    $dbuser = "root";
+    $db = "pokemon";
+    $con = mysqli_connect($dbhost, $dbuser, "", $db);
+    $pokemon_name = $_GET["pokemon_name"];
+
+    $query = "SELECT entry_id, pokemonid, name, type1, type2, totalstat, hp, attack, defense, specialattack, specialdefense, speed, generation, legendarystatus FROM pokemon_data WHERE (name = '$pokemon_name')";
+
+    $result = mysqli_query($con, $query);
+
+    $array = mysqli_fetch_row($result);
+
+    mysqli_close($con);
+
+    echo json_encode($array);
+?>

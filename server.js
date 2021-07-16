3feba0788976295593eae56bdf39c161aab6800d
jb1685 <joshua.caleb.bender@gmail.com>
2021-07-15 23:33:50 -0400

    updated server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 8080;
 /* middleware */
 app.use(express.static('/Users/joshbender/Dropbox/College/Summer 2021/CMSC 433/projects/CMSC-433-proj3/CMSC-433-Pokemon-Game/'));
 
+/* api */
+app.get("/", function (req, res) {
+    res.redirect("/proj3.html");
+});
+
 /* server */
 app.listen(port);
 
